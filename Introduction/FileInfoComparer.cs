1f015666137d73e338aeb20080232a7d5c97c5cf
Kasthuri Mahadeva <kasthuri@unicomtechnology.co.uk>
2020-05-30 03:48:12 +0530

    linq introduction

---
+++ Introduction/FileInfoComparer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Introduction
+{
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo file1, FileInfo file2)
+        {
+            if (file1 != null && file2 != null)
+            {
+                return file2.Length.CompareTo(file1.Length);
+            }
+            return 0;
+        }
+    }
+}
---
+++ Introduction/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Windows";
+            FileCompareWithoutLinq(path);
+            Console.WriteLine("*************");
+            FileCompareWithLinq(path);
+        }
+
+        private static void FileCompareWithLinq(string path)
+        {
+            // var query1 = from file in new DirectoryInfo(path).GetFiles() 
+            //     orderby file.Length descending select file;
+            //
+            // foreach (var fileInfo in query1.Take(5))
+            // {
+            //     Console.WriteLine($"{fileInfo.Name, -25} : {fileInfo.Length, 15:N0}");
+            // }
+            
+            var query = Directory.GetFiles(path).Select(f => new FileInfo(f)).
+                OrderByDescending(f => f.Length).Take(5);
+            foreach (var fileInfo in query)
+            {
+                Console.WriteLine($"{fileInfo.Name, -25} : {fileInfo.Length, 15:N0}");
+            }
+        }
+
+        private static void FileCompareWithoutLinq(string path)
+        {
+            var directory = new DirectoryInfo(path);
+            var files = directory.GetFiles();
+            // Array.Sort(files, (x, y) => 
+            //     (int) (y.Length - x.Length));
+            Array.Sort(files, new FileInfoComparer());
+            
+            for (var i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{files[i].Name, -25} : {files[i].Length, 15:N0}");
+            }
+        }
+    }
+}
